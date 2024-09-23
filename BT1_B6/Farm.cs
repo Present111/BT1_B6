e17647b4f0f9f795f68bf9807cf6cfd8301d0469
Present111 <22521118@gm.uit.edu.vn>
2024-09-23 09:49:52 +0700

    Viet farm quan ly

--- BT1_B6/Farm.cs
+++ BT1_B6/Farm.cs
@@ -8,5 +8,30 @@ namespace BT1_B6
 {
     internal class Farm
     {
+        public Cow Cows { get; set; }
+        public Sheep Sheep { get; set; }
+        public Goat Goats { get; set; }
+
+        public Farm(int cows, int sheep, int goats)
+        {
+            Cows = new Cow(cows);
+            Sheep = new Sheep(sheep);
+            Goats = new Goat(goats);
+        }
+
+        public string GetSounds()
+        {
+            return $"{Cows.MakeSound()} {Sheep.MakeSound()} {Goats.MakeSound()}";
+        }
+
+        public int TotalMilk()
+        {
+            return Cows.Milk() + Sheep.Milk() + Goats.Milk();
+        }
+
+        public int TotalOffspring()
+        {
+            return Cows.GiveBirth() + Sheep.GiveBirth() + Goats.GiveBirth();
+        }
     }
 }
